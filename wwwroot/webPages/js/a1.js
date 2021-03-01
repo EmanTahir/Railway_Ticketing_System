e6f7f56468ffe6b382740746687c13c0300fbfcf
EmanTahir <55186684+EmanTahir@users.noreply.github.com>
2021-03-01 23:12:58 +0500

    Add files via upload

---
+++ wwwroot/webPages/js/a1.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('#dt-multi-checkbox').dataTable({
+
+    columnDefs: [{
+      orderable: false,
+      className: 'select-checkbox',
+      targets: 0
+    }],
+    select: {
+      style: 'multi',
+      selector: 'td:first-child'
+    }
+  });
+});
